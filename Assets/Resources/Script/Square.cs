e46436ca0920abdb1de22c3ebc18996a686d8c44
khkh3203 <khkh3203@naver.com>
2017-08-30 21:36:46 +0900

    2048Assets

---
+++ Assets/Resources/Script/Square.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    private bool move = false;
+    private bool combine = false;
+    public float speed = 0.2f;
+    public int value;
+    Vector2 curTarget;
+    GameObject combineTarget;
+    Color[] curColorArray;
+    
+    private void Start()
+    {
+        value = System.Convert.ToInt32(transform.FindChild("Text").GetComponent<TextMesh>().text);
+        combineTarget = null;
+    }
+
+    public bool isMoving()
+    {
+        return move;
+    }
+
+    public bool Combine
+    {
+        get
+        {
+            return combine;
+        }
+
+        set
+        {
+            combine = value;
+        }
+    }
+
+    private void Update()
+    {
+        if(move)
+        {
+            if(combineTarget != null)
+            {
+                Move(combineTarget);
+            }
+            else
+            {
+                Move(curTarget);
+            }
+        }
+    }
+
+    private int getPow(int value)
+    {
+        int count = 0;
+
+        while(true)
+        {
+            value /= 2;
+            count++;
+
+            if(value == 1) { break; }
+        }
+
+        return count;
+    }
+
+    public void Move(Vector2 target)
+    {
+        move = true;
+        curTarget = target;
+        transform.position = Vector2.MoveTowards(transform.position, target, speed);
+
+        if (curTarget == (Vector2)transform.position)
+        {
+            move = false;
+            GameObject.Find("SquareManager").GetComponent<SquareManager>().CreateSquare();
+        }
+    }
+
+    public void Move(GameObject target)
+    {
+        move = true;
+        combineTarget = target;
+
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed);
+
+        if (target.transform.position == transform.position)
+        {
+            move = false;
+            target.transform.FindChild("Text").GetComponent<TextMesh>().text = target.GetComponent<Square>().value.ToString();
+            target.GetComponent<SpriteRenderer>().color = curColorArray[getPow(target.GetComponent<Square>().value) - 1];
+            GameObject.Find("SquareManager").GetComponent<SquareManager>().CreateSquare();
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetColor(Color[] colorArray)
+    {
+        curColorArray = colorArray;
+        string numberText = transform.FindChild("Text").GetComponent<TextMesh>().text;
+        int number = System.Convert.ToInt32(numberText);
+
+        GetComponent<SpriteRenderer>().color = colorArray[(number / 2) - 1];
+    }
+}
---
+++ Assets/Resources/Script/SquareManager.cs
@@ -0,0 +1,300 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareManager : MonoBehaviour
+{
+    Color[] colorArray;
+    public GameObject defaultSquare;
+    bool created = false;
+    bool dragging;
+    bool full;
+    Vector3 zeroIndexPosition;
+    GameObject[,] squareArray;
+    int topY = 0;
+
+    private void Awake()
+    {
+        colorArray = new Color[10];
+        for (int i = 0; i < 10; i++)
+        {
+            colorArray[i] = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+        }
+
+        dragging = false;
+        full = false;
+        zeroIndexPosition = defaultSquare.transform.position;
+        squareArray = new GameObject[4, 4];
+
+    }
+
+    private void Start()
+    {
+        CreateSquare();
+    }
+
+    private void Update()
+    {
+        CheckArrayFull();
+        bool check = false;
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            created = false;
+            CreateSquare();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+            for(int y = 0; y < 4; y++)
+            {
+                for(int x = 0; x < 4; x++)
+                {
+                    if(squareArray[y, x] == null) { continue; }
+                    squareArray[y, x].GetComponent<Square>().Combine = false;
+                }
+            }
+        }
+
+        if (dragging) { return; }
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (squareArray[i, j] == null) { continue; }
+                if (squareArray[i, j].GetComponent<Square>().isMoving()) { return; }
+            }
+        }
+        
+        switch (SwipeManager.swipeDirection)
+        {
+            case Swipe.Up:
+                dragging = true;
+                check = true;
+                for (int y = 1; y < 4; y++)
+                {
+                    for (int x = 0; x < 4; x++)
+                    {
+                        if (squareArray[y, x] == null) { continue; }
+
+                        for (int i = y - 1; i >= 0; i--)
+                        {
+                            if (squareArray[i, x] != null && !squareArray[i, x].GetComponent<Square>().Combine)
+                            {
+                                int number = squareArray[i, x].GetComponent<Square>().value;
+                                int number2 = squareArray[y, x].GetComponent<Square>().value;
+
+                                if (number == number2)
+                                {
+                                    squareArray[i, x].GetComponent<Square>().value = number + number2;
+                                    squareArray[i, x].GetComponent<Square>().Combine = true;
+                                    squareArray[y, x].GetComponent<Square>().Move(squareArray[i, x]);
+                                    squareArray[y, x] = null;
+                                    full = false;
+
+                                    break;
+                                }
+
+                                break;
+                            }
+
+                            if (squareArray[i, x] == null)
+                            {
+                                topY = i;
+                            }
+                        }
+                        if (squareArray[topY, x] == null && squareArray[y, x] != null)
+                        {
+                            squareArray[topY, x] = squareArray[y, x];
+                            squareArray[y, x] = null;
+                            squareArray[topY, x].GetComponent<Square>().Move(new Vector2(zeroIndexPosition.x + (0.6f * x - topY + (x * 0.4f)), zeroIndexPosition.y - (0.6f * topY + x + (topY * 0.4f))));
+                            full = false;
+                        }
+                    }
+                }
+                if (check) { created = false; }
+                break;
+            case Swipe.Down:
+                dragging = true;
+                check = true;
+                for (int y = 2; y >= 0; y--)
+                {
+                    for (int x = 0; x < 4; x++)
+                    {
+                        if (squareArray[y, x] == null) { continue; }
+
+                        for (int i = y + 1; i < 4; i++)
+                        {
+                            if (squareArray[i, x] != null && !squareArray[i, x].GetComponent<Square>().Combine)
+                            {
+                                int number = squareArray[i, x].GetComponent<Square>().value;
+                                int number2 = squareArray[y, x].GetComponent<Square>().value;
+
+                                if (number == number2)
+                                {
+                                    squareArray[i, x].GetComponent<Square>().value = number + number2;
+                                    squareArray[i, x].GetComponent<Square>().Combine = true;
+                                    squareArray[y, x].GetComponent<Square>().Move(squareArray[i, x]);
+                                    squareArray[y, x] = null;
+                                    full = false;
+                                    break;
+                                }
+
+                                break;
+                            }
+
+                            if (squareArray[i, x] == null)
+                            {
+                                topY = i;
+                            }
+                        }
+                        if (squareArray[topY, x] == null && squareArray[y, x] != null)
+                        {
+                            squareArray[topY, x] = squareArray[y, x];
+                            squareArray[y, x] = null;
+                            squareArray[topY, x].GetComponent<Square>().Move(new Vector2(zeroIndexPosition.x + (0.6f * x - topY + (x * 0.4f)), zeroIndexPosition.y - (0.6f * topY + x + (topY *0.4f))));                            
+                            full = false;
+                        }
+                    }
+                }
+                if (check) { created = false; }
+                break;
+            case Swipe.Left:
+                dragging = true;
+                check = true;
+                for (int y = 0; y < 4; y++)
+                {
+                    for (int x = 1; x < 4; x++)
+                    {
+                        if (squareArray[y, x] == null) { continue; }
+
+                        for (int i = x - 1; i >= 0; i--)
+                        {
+                            if (squareArray[y, i] != null && !squareArray[y, i].GetComponent<Square>().Combine)
+                            {
+                                int number = squareArray[y, i].GetComponent<Square>().value;
+                                int number2 = squareArray[y, x].GetComponent<Square>().value;
+
+                                if (number == number2)
+                                {
+                                    squareArray[y, i].GetComponent<Square>().value = number + number2;
+                                    squareArray[y, i].GetComponent<Square>().Combine = true;
+                                    squareArray[y, x].GetComponent<Square>().Move(squareArray[y, i]);
+                                    squareArray[y, x] = null;
+                                    full = false;
+                                    break;
+                                }
+
+                                break;
+                            }
+
+                            if (squareArray[y, i] == null)
+                            {
+                                topY = i;
+                            }
+                        }
+                        if (squareArray[y, topY] == null && squareArray[y, x] != null)
+                        {
+                            squareArray[y, topY] = squareArray[y, x];
+                            squareArray[y, x] = null;
+                            squareArray[y, topY].GetComponent<Square>().Move(new Vector2(zeroIndexPosition.x + (0.6f * topY - y + (topY * 0.4f)), zeroIndexPosition.y - (0.6f * y + topY + (y * 0.4f))));
+                            full = false;
+                        }
+                    }
+                }
+                if (check) { created = false; }
+                break;
+            case Swipe.Right:
+                dragging = true;
+                check = true;
+                for (int y = 0; y < 4; y++)
+                {
+                    for (int x = 2; x >= 0; x--)
+                    {
+                        if (squareArray[y, x] == null) { continue; }
+
+                        for (int i = x + 1; i < 4; i++)
+                        {
+                            if (squareArray[y, i] != null && !squareArray[y, i].GetComponent<Square>().Combine)
+                            {
+                                int number = squareArray[y, i].GetComponent<Square>().value;
+                                int number2 = squareArray[y, x].GetComponent<Square>().value;
+
+                                if (number == number2)
+                                {
+                                    squareArray[y, i].GetComponent<Square>().value = number + number2;
+                                    squareArray[y, i].GetComponent<Square>().Combine = true;
+                                    squareArray[y, x].GetComponent<Square>().Move(squareArray[y, i]);
+                                    squareArray[y, x] = null;
+                                    full = false;
+                                    break;
+                                }
+
+                                break;
+                            }
+
+                            if (squareArray[y, i] == null)
+                            {
+                                topY = i;
+                            }
+                        }
+                        if (squareArray[y, topY] == null && squareArray[y, x] != null)
+                        {
+                            squareArray[y, topY] = squareArray[y, x];
+                            squareArray[y, x] = null;
+                            squareArray[y, topY].GetComponent<Square>().Move(new Vector2(zeroIndexPosition.x + (0.6f * topY - y + (topY * 0.4f)), zeroIndexPosition.y - (0.6f * y + topY + (y * 0.4f))));                            
+                            full = false;
+                        }
+                    }
+                }
+                if (check) { created = false; }
+                break;
+            default:
+                return;
+        }
+    }
+
+    private void CheckArrayFull()
+    {
+        int check = 0;
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (squareArray[i, j] == null) { continue; }
+                check++;
+            }
+        }
+
+        if (check >= 16) { full = true; }
+    }
+
+    /// <summary>
+    /// Creates a Square at the random index of the array
+    /// </summary>
+    public void CreateSquare()
+    {
+        if (full) { return; }
+        if(created) { return; }
+        created = true;
+
+        int x = 0;
+        int y = 0;
+        while (true)
+        {
+            x = Random.Range(0, 4);
+            y = Random.Range(0, 4);
+            if (squareArray[y, x] == null) { break; }
+        }
+
+        squareArray[y, x] = Instantiate(defaultSquare, new Vector3(zeroIndexPosition.x + (0.6f * x-y+(x*0.4f)), zeroIndexPosition.y - (0.6f * y+x+(y*0.4f)), zeroIndexPosition.z), Quaternion.identity);
+//        squareArray[y, x] = Instantiate(defaultSquare, new Vector3(zeroIndexPosition.x + (1.2f * x), zeroIndexPosition.y - (1.2f * y), zeroIndexPosition.z), Quaternion.identity);
+        squareArray[y, x].transform.FindChild("Text").GetComponent<TextMesh>().text = (Random.Range(0, 4) > 2) ? 4.ToString() : 2.ToString();
+        squareArray[y, x].GetComponent<Square>().SetColor(colorArray);
+        squareArray[y, x].transform.Rotate(new Vector3(0, 0, -45));
+
+    }
+}
